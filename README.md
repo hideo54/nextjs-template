e230e4d00279cc7a9ffac5fccbd775a7d6c06aa8
Hideo Yasumoto <hideo54@hideo54.com>
2021-11-21 05:33:46 +0900

    Introduce @hideo54/reactor

--- README.md
+++ README.md
@@ -7,5 +7,10 @@
         * header には JS 無効化や IE への警告つき
     * Google Analytics
     * Google Fonts (San Francisco > ヒラギノ > Noto Sans Japanese)
+    * [@hideo54/reactor](https://github.com/hideo54/reactor)
 * TypeScript
 * styled-components
+
+## 注意点
+
+* hideo54 で検索し、適宜 placeholder を書き換えること。
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link';
 import styled from 'styled-components';
+import { Sparkles } from '@styled-icons/ionicons-outline';
+import { IconLink } from '@hideo54/reactor';
 import Layout from '../components/Layout';
 
 const H2 = styled.h1`
@@ -9,6 +12,9 @@ const App: React.FC = () => {
     return (
         <Layout>
             <H2>Hello, world!</H2>
+            <Link href='/' passHref>
+                <IconLink LeftIcon={Sparkles}>最高のテンプレート</IconLink>
+            </Link>
             <p>こんにちは</p>
         </Layout>
     );
